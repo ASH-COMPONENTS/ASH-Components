404d21d0e626d42eb7b965432d2fdc56d3285409
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-05-26 16:42:33 +0300

    Solve some problems and add buttons to control-2

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,18 @@
-var arVideos = document.getElementsByTagName('video');
+let gotop = document.querySelector(".go-top");
 
-for (var i = arVideos.length - 1; i >= 0; i--) {
-	var elmVideo = arVideos[i];
-	elmVideo.autoplay = true;
+window.onscroll = function () {
+	// console.log(this.scrollY); // show number scroll
+	if (this.scrollY >= 900) {
+		gotop.classList.add("show")
+	} else {
+		gotop.classList.remove("show")
+	}
+};
+
+// go to top
+gotop.onclick = function () {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	})
 }
