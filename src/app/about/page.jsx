f16584c81b9344acd261c4ab7edb1daaeddbdbdd
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-01-20 23:05:46 +0200

    add react icons

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,27 +1,16 @@
-"use client"
+async function aboutus() {
+let api = 'https://jsonplaceholder.typicode.com/todos/1';
+let response = await fetch(api);
+const todo = await response.json();
 
-import { useState, useEffect } from "react"
-
-// export const metadata = {
-//   description: "this page make to show all hellpes site and show all futures"
-// }
-
-const aboutus = () => {
-  let [product, setProduct] = useState({});
-  let api = 'https://jsonplaceholder.typicode.com/todos/1'
-  useEffect(() =>{
-    fetch(api).then((response) => response.json()).then((response) => {
-      console.log(response);
-      setProduct(response);
-    });
-  }, []);
   return (
     <>
     <h1>hello on about todos</h1>
     <div>
         <h1>
-          {product.title}
+          {todo.id}
         </h1>
+          <h1>{todo.title}</h1>
     </div>
     </>
   )
