36f60d845b0c40b0a1a4a2ec3990555e14dc3fdd
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-01-20 15:44:06 +0200

    navgation end

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,29 @@
-export const metadata = {
-  description: "this page make to show all hellpes site and show all futures"
-}
+"use client"
+
+import { useState, useEffect } from "react"
+
+// export const metadata = {
+//   description: "this page make to show all hellpes site and show all futures"
+// }
 
 const aboutus = () => {
+  let [product, setProduct] = useState({});
+  let api = 'https://jsonplaceholder.typicode.com/todos/1'
+  useEffect(() =>{
+    fetch(api).then((response) => response.json()).then((response) => {
+      console.log(response);
+      setProduct(response);
+    });
+  }, []);
   return (
-    <h1>hello world on about</h1>
+    <>
+    <h1>hello on about todos</h1>
+    <div>
+        <h1>
+          {product.title}
+        </h1>
+    </div>
+    </>
   )
 }
 
--- src/app/components/navgationbar/page.jsx
+++ src/app/components/navgationbar/page.jsx
@@ -4,12 +4,25 @@ import styles from './page.module.css';
 const Nav = () => {
     return (
         <div className={styles.navgationBar}>
-            <h3 className={styles.brand} >ASH-EDUCATION</h3>
-            <nav className={styles.links}>
-                <Link href={'/'} className={styles.link}>home</Link>
-                <Link href={'/about'} className={styles.link}>about-us</Link>
-                <Link href={'/'} className={styles.link}>services</Link>
-            </nav>
+            <div className='container' style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                <div className={styles.nav}>
+                    <h3 className={styles.brand} >ASH-EDUCATION</h3>
+                    <nav className={styles.links}>
+                        <Link href={'/'} className={styles.link}>home</Link>
+                        <Link href={'/about'} className={styles.link}>about-us</Link>
+                        <Link href={'/'} className={styles.link}>
+                            cources
+                            <div className={styles.tracks}>
+                                
+                            </div>
+                        </Link>
+                        
+                    </nav>
+                </div>
+                    <div className={styles.contactUs}>
+                        <Link href={''}><button className={styles.contact}>Contact Us</button></Link>
+                    </div>
+            </div>
         </div>
     )
 }
