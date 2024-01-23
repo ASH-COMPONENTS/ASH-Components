828aeab3366e1bb4e20bb7f649ac9c8b531f115b
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-01-23 03:05:37 +0200

    end landing page

---
+++ src/app/components/trackes/Tracks.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import style from './page.module.css'
+
+const Tracks = (props) => {
+    return (
+        <div className={style.trackBox} >
+            <h3 className={style.name} >{props.trackName}</h3>
+            <div>
+                <h3 className={style.trackInfo}>{props.trackManyInfo}</h3>
+                <Link className={style.LinkTrack} href={props.LinkTrack}>{props.linkName}</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Tracks
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,40 @@
-import Image from 'next/image'
+import Tracks from './components/trackes/tracks';
 import styles from './page.module.css'
 
-export default function Home() {
+function Home() {
   return (
-    <div className="homePage" id='homePage' >
+    <div className={styles.homePage} id='homePage'>
       <div className="container">
-        
+        <div className={styles.textContent}>
+          <h1 className={styles.title}>welom to <span>ASH-</span>foundation for education </h1>
+          <p className={styles.description}>
+            You can get a lot of types of Software fields such as front end, back end and fullstack and you can get offline courses.
+          </p>
+
+          <div className='tracks' style={{display: 'flex'}}>
+            <Tracks
+              trackName={'javaScript'}
+              trackManyInfo={'made to make web site more dinamic and show information from clint side and server side, show all info'}
+              LinkTrack={'/about'}
+              linkName={'go to learn'}
+            />
+            <Tracks
+              trackName={'typeScript'}
+              trackManyInfo={'this is the javascript with data type syntax'}
+              LinkTrack={'/'}
+              linkName={'go to learn'}
+            />
+            <Tracks
+              trackName={'nodeJs'}
+              trackManyInfo={'this is a javascript run time workig in server side "backEnd"'}
+              LinkTrack={'/'}
+              linkName={'go to learn'}
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
 }
+
+export default Home;
